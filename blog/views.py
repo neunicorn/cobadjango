ef999cd89812090f2f631e5a716846ced287daf9
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2021-11-28 21:05:18 +0700

    menambahkan database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'zulfan',
-        'title' : 'blog post 1',
-        'content': 'first post content',
-        'date' : 'June 12, 2020'
-    },
-    {
-        'author': 'rapli',
-        'title' : 'blog post 2',
-        'content': 'second post content',
-        'date' : 'June 13, 2020'
-    }
-]
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
