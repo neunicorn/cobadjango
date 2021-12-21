71ed52bae5c69033f76529ad11e5c1d10aed5f99
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2021-12-21 22:06:57 +0700

    menambahkan login dan logout, dan membuat halaman yang bisa diaakses saat sudah login saja

--- cobadjango/urls.py
+++ cobadjango/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 import debug_toolbar
 from users import views as user_views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('ukmuinjkt/', include('ukmuinjkt.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
